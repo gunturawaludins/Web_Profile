f88846c610ac96d1f99ea19a143922e09e6b9566
gunturawaludins <gunturawaludinspt@gmail.com>
2024-11-03 23:58:12 +0700

    Save current changes before rebase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
 import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
-import '@/styles/globals.css';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -13,14 +12,15 @@ const montserrat = Montserrat({
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-    <main className={`${montserrat.variable} font-mont bg-light w-full dark:bg-dark min-h-screen`}>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-    </main></>
-  )
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrat.variable} font-mont bg-light w-full dark:bg-dark min-h-screen`}>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  );
 };
